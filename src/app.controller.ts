9322038d467ad327e70f6220c1864ff643469702
SujalNayak <121352118+SujalNayak@users.noreply.github.com>
2024-05-26 11:59:47 +0530

    may 26

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
+import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from './multer.config';
+import { v2 as cloudinary } from 'cloudinary';
+import { Express } from 'express';
+
+
 
 @ApiTags("users")
 @Controller()
@@ -22,4 +29,30 @@ export class AppController {
     console.log(req.body);
     return this.appService.addUser(req.body);
   }
+  cloudinaryHandler = async (file) => {
+    cloudinary.config({
+      cloud_name: "dzjnlpzak",
+      api_key: "664615118614288",
+      api_secret: "JYyneoiGdSLRNtFz5BXTORyxHTQ"
+    })
+
+    return await cloudinary.uploader.upload(file.path);
+
+  }
+
+    
+  @Post("/file")
+  @UseInterceptors(FileInterceptor("file", multerConfig))
+  async handleUpload(@UploadedFile() file: Express.Multer.File) {
+    try {
+      console.log(file);
+      var result = await this.cloudinaryHandler(file);
+      console.log(result);
+      return file;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
 }
+
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from 'src/schemas/User.schema';
 import { Model } from 'mongoose';
-import { SignupDto } from './dto/SignupDto.dto';
+import { User } from 'src/schemas/user.schema';
 import * as bcrypt from 'bcryptjs';
-
-
+import { JwtService } from '@nestjs/jwt/dist';
+import { SignupDto } from './dto/SignupDto.dto';
+import { LoginDto } from './dto/LoginDto.dto';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
+
     constructor(
-        @InjectModel(User.name) private userModel: Model<User>,
-        private jwtService: JwtService
+        @InjectModel(User.name) private userModel:Model<User>,
+        private jwtService:JwtService
     ){}
 
     async signUp(signupDto:SignupDto){
@@ -29,5 +30,27 @@ export class AuthService {
         const token = this.jwtService.sign({id:user.name});
 
         return {token};
+    }
+
+    async login(loginDto:LoginDto):Promise<{token:string}>{
+
+        const {email,password} = loginDto;
+        const user = await this.userModel.findOne({email})
+        if(!user){
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+        if(!isPasswordMatch){
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const token = this.jwtService.sign({id:user._id});
+        return {token};
+    }
+
+    validateToken(token:string){
+        return this.jwtService.verify(token,{
+            secret:process.env.JWT_SECRET
+        });
     }
 }
